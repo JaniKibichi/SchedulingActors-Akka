20b421f9bdcad7e29063979e93d0bd9369d9e1c7
janikibichi <janikibichi@gmail.com>
2018-05-26 17:48:08 +0300

    complete docker running container

--- dockerApp/dockerize.sh
+++ dockerApp/dockerize.sh
@@ -5,7 +5,10 @@ rm -rf ../target && rm -rf docker/*
 echo "=================================Entering schedulingActors directory, creating assembly ==>"
 cd ../ && sbt assembly
 echo "=====================================================Coming back to dockerApp directory ==>"
-cd - echo "==========================================Copying application jar to docker folder ==>"
+cd dockerApp
+echo "===============================================Copying application jar to docker folder ==>"
 sudo cp ../target/scala-2.11/SchedulingActors-Akka-assembly-1.0.jar docker/
+echo "==================================================================Building Docker Image ==>"
+sudo docker build -t akkaapp .
 echo "===============================================================Running docker container ==>"
 sudo docker run akkaapp
