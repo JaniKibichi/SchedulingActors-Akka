2d40e760dbd46fea6a56b868665d0cb4729dae7a
janikibichi <janikibichi@gmail.com>
2018-05-26 14:28:22 +0300

    update logging with actor

--- src/main/scala/com/github/janikibichi/learnakka/schedulingActors/LoggingWithActors.scala
+++ src/main/scala/com/github/janikibichi/learnakka/schedulingActors/LoggingWithActors.scala
@@ -1,5 +1,30 @@
 package com.github.janikibichi.learnakka.schedulingActors
 
-object LoggingWithActors extends App {
+import akka.actor.{Props, ActorSystem, ActorLogging, Actor}
 
+object LoggingWithActors extends App {
+  val actorSystem = ActorSystem("LoggingWithActors")
+
+  //Create and define an Actor in the ActorSystem
+  val actor = actorSystem.actorOf(Props[LoggingActor], "SumActor")
+
+  //Send messages to the Actors
+  actor ! (10, 12)
+  actor ! "Hello dude!!!!"
+
+  //Terminate
+  actorSystem.terminate()
+
+}
+
+//Create an Actor with Logging
+class LoggingActor extends Actor with ActorLogging {
+  def receive = {
+    case (a: Int, b: Int) => {
+      log.info(s"Sum of $a and $b is ${a+b}")
+    }
+
+    case msg =>
+      log.warning(s" i dont know what you are talking about : $msg")
+  }
 }
