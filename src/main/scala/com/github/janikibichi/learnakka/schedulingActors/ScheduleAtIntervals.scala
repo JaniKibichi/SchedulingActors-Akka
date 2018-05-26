870f51815ab324aebd2e793892f06fd71a473c7d
janikibichi <janikibichi@gmail.com>
2018-05-26 12:49:03 +0300

    set up scheduler

---
+++ src/main/scala/com/github/janikibichi/learnakka/schedulingActors/ScheduleAtIntervals.scala
@@ -0,0 +1,30 @@
+package com.github.janikibichi.learnakka.schedulingActors
+
+import akka.actor.{Actor, Props, ActorSystem}
+import scala.concurrent.duration._
+
+//App that Schedules the Actor
+object  ScheduleAtIntervals extends App {
+  val actorSystem=ActorSystem("ScheduleAtIntervals")
+  //Import the dispatcher
+  import actorSystem.dispatcher
+
+  //Create and define an Actor in the ActorSystem
+  val actor = actorSystem.actorOf(Props[RandomIntAdder])
+
+  actorSystem.scheduler.scheduleOnce(10 seconds, actor, "tick")
+  actorSystem.scheduler.schedule(11 seconds, 2 seconds, actor,"tick")
+}
+
+//Create an Actor that adds 2 random integers
+class RandomIntAdder extends Actor {
+  val random = scala.util.Random
+
+  def receive = {
+    case "tick" =>
+      val randomInta = random.nextInt(10)
+      val randomIntb = random.nextInt(10)
+
+      println(s"sum of $randomInta and $randomIntb is ${randomInta+randomIntb}")
+  }
+}
